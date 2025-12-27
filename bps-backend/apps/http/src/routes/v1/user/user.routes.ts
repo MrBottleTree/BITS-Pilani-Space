3e1c0c3f34f5f379af586baf451dfe100a8a8a5c
Vishrut Ramraj <vishrut172@gmail.com>
2025-12-27 18:08:30 +0530

    modified backend to accept the DELETE http method

--- bps-backend/apps/http/src/routes/v1/user/user.routes.ts
+++ bps-backend/apps/http/src/routes/v1/user/user.routes.ts
@@ -4,6 +4,6 @@ import * as middleware from "../middleware/user.js";
 
 const router = Router();
 
-router.post("/delete", middleware.user_simple_middleware, middleware.user_strong_middleware, userController.delete_post);
+router.delete("/me", middleware.user_simple_middleware, middleware.user_strong_middleware, userController.delete_user);
 
 export default router;
