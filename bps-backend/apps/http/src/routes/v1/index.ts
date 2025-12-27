86ff75dfc713ef4fe8c4d6f860db02c41e590959
Vishrut Ramraj <vishrut172@gmail.com>
2025-12-27 12:18:08 +0530

    initialized users route

--- bps-backend/apps/http/src/routes/v1/index.ts
+++ bps-backend/apps/http/src/routes/v1/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import authRouter from "./auth/auth.routes";
+import userRouter from "./user/user.controller";
 
 const router = Router();
 
@@ -9,5 +10,6 @@ router.get("/healthz", (req, res) => {
 });
 
 router.use("/auth", authRouter);
+router.use("/user", userRouter);
 
 export default router;
