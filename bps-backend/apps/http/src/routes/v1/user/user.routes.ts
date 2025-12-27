a23462f862c16d6ad6565d02ff655ef3e541a748
Vishrut Ramraj <vishrut172@gmail.com>
2025-12-27 21:01:41 +0530

    modified user routes

--- bps-backend/apps/http/src/routes/v1/user/user.routes.ts
+++ bps-backend/apps/http/src/routes/v1/user/user.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import * as userController from "./user.controller.js";
-import * as middleware from "../middleware/user.js";
+import * as middleware from "../middleware/utils/util.middleware.js";
 
 const router = Router();
 
-router.delete("/me", middleware.user_simple_middleware, middleware.user_strong_middleware, userController.delete_user);
+router.delete("/me", middleware.strong_middleware("USER"), userController.delete_user);
 
 export default router;
