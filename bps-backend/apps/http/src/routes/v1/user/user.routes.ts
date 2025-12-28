4d4600d137000ae5bf946ea83a4ca6145807275d
Vishrut Ramraj <vishrut172@gmail.com>
2025-12-28 22:18:22 +0530

    routes

--- bps-backend/apps/http/src/routes/v1/user/user.routes.ts
+++ bps-backend/apps/http/src/routes/v1/user/user.routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 
 router.delete("/me", middleware.strong_middleware("USER"), userController.delete_user);
 router.patch("/me", middleware.strong_middleware("USER"), userController.update_user);
+router.get("/me", middleware.simple_middleware("USER"), userController.get_user);
 
 export default router;
