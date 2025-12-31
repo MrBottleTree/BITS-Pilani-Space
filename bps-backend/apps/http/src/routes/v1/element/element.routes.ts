711860b7991091bfdf54dd55b3917b2eab7313b7
Vishrut Ramraj <vishrut172@gmail.com>
2025-12-31 20:22:46 +0530

    slight modifications

--- bps-backend/apps/http/src/routes/v1/element/element.routes.ts
+++ bps-backend/apps/http/src/routes/v1/element/element.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import * as elementController from "./elements.controller.js";
+import * as elementController from "./element.controller.js";
 import { simple_middleware } from "../middleware/utils/util.middleware.js";
 
 const router = Router();
