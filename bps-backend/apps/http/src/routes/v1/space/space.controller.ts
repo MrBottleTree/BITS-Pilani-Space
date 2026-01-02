a8bd014c608cd0a6f2f6ba47c82733f5e58dce7d
Vishrut Ramraj <vishrut172@gmail.com>
2026-01-02 14:06:25 +0530

    adding space made

--- bps-backend/apps/http/src/routes/v1/space/space.controller.ts
+++ bps-backend/apps/http/src/routes/v1/space/space.controller.ts
@@ -1,5 +1,85 @@
 import { NextFunction, Request, Response } from "express";
+import * as Types from "../../../types/index.js";
+import { ERROR_DATABASE_CONNECT_FOREIGN, HTTP_STATUS } from "../../../config.js";
+import { get_parsed_error_message } from "../utils/helper.js";
+import { client } from "@repo/db";
 
 export const add_space = async (req: Request, res: Response, next: NextFunction) => {
+    const parsed_body = Types.AddSpaceSchema.safeParse(req.body);
+    if(!parsed_body.success){
+        res.status(HTTP_STATUS.BAD_REQUEST).json({"error": "Error parsing the body", "details": get_parsed_error_message(parsed_body)});
+        return
+    }
+
+    try {
+        const map_id = parsed_body.data.map_id;
+        const name = parsed_body.data.name;
+
+        const check_map = client.map.findUnique({
+            where: {id: map_id, deleted_at: null},
+            select: { id: true },
+        });
+
+        if(!check_map){
+            res.status(HTTP_STATUS.BAD_REQUEST).json({error: "Map deleted."});
+            return;
+        }
+
+        const db_response = client.space.create({
+            data: {
+                name: name,
+                map: { connect: {id: map_id} }
+            },
+            select: {
+                id: true,
+                created_at: true,
+                updated_at: true,
+                map: {
+                    select: {
+                        id: true,
+                        name: true,
+                        height: true,
+                        width: true,
+                        thumbnail_key: true,
+                        updated_at: true,
+                        default_elements: {
+                            select: {
+                                id: true,
+                                x: true,
+                                y: true,
+                                scale: true,
+                                rotation: true,
+                                element: {
+                                    select: {
+                                        id: true,
+                                        name: true,
+                                        image_key: true,
+                                        height: true,
+                                        width: true,
+                                        static: true,
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        if(!db_response) return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR);
+
+        res.status(HTTP_STATUS.CREATED).json({message:'Space created.', data: { space: db_response }});
+    }
+    catch(err: any){
+        if(err.code == ERROR_DATABASE_CONNECT_FOREIGN){
+            res.status(HTTP_STATUS.BAD_REQUEST).json({error: "Invalid map id."});
+            return;
+        }
+
+        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR);
+    }
+};
+
+export const add_element_to_space = async (req: Request, res: Response, next: NextFunction) => {
     
 };
--- bps-backend/apps/http/src/routes/v1/space/space.routes.ts
+++ bps-backend/apps/http/src/routes/v1/space/space.routes.ts
@@ -5,5 +5,6 @@ import { simple_middleware } from "../middleware/utils/util.middleware.js";
 const router = Router();
 
 router.post("/", simple_middleware("USER"), spaceController.add_space);
+router.post("/add", simple_middleware("USER"), spaceController.add_element_to_space)
 
 export default router;
