53a970fbea5db79b0761cb8f4509f79f8a847ef0
Vishrut Ramraj <vishrut172@gmail.com>
2026-01-03 18:23:38 +0530

    avatar testcases initialization

---
+++ tests/avatar/success.test.js
@@ -0,0 +1,11 @@
+const utils = require("../utils/testUtils.js");
+
+const valid_password = "Password@123";
+
+describe("Unit tests", () => {
+    test("Admin can upload avatar", async () => {});
+
+    test("Anyone can get a specific avatar", async () => {});
+
+    test("Anyone can get the list of all avatars", async () => {});
+});
