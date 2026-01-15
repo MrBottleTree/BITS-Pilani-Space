250789f7c274b01f994646425352430981790eed
Vishrut Ramraj <vishrut172@gmail.com>
2026-01-16 00:28:20 +0530

    Signin frontend works
    It only logs things, need to store things in storage

---
+++ bps-backend/apps/web/src/pages/Signin.tsx
@@ -0,0 +1,53 @@
+import axios, { HttpStatusCode } from "axios";
+import { useState } from "react"
+import { HTTP_BACKEND_URL } from "../config";
+import { Input } from "../components/ui/Input";
+import { Button } from "../components/ui/Button";
+
+export function Signin() {
+    const [identifier, setIdentifier] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    async function handleSignin() {
+        setIsLoading(true);
+
+        try{
+            const response = await axios.post(`${HTTP_BACKEND_URL}/api/v1/auth/signin`, {identifier, password});
+
+            if(response.status == HttpStatusCode.Ok){
+                console.log(response.data);
+                // Need to store the access key and stuff in local storage
+            }
+            else{
+                // Need to take care of cases like incorrect identifier etc
+                alert("Error in signing in")
+            }
+        }
+
+        catch(err) {
+            console.error("Error: " + err);
+        }
+
+        finally{
+            setIsLoading(false);
+        }
+    }
+
+    return <div>
+        <h1>Sign In</h1>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSignin();
+        }}>
+            <Input placeholder="Handle or Email" onChange={(e) => setIdentifier(e.target.value)} />
+            <br />
+            <Input placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <br />
+            <Button 
+                label={isLoading ? "Signing in..." : "Submit"} 
+                disabled={isLoading}
+            />
+        </form>
+    </div>
+};
--- bps-backend/apps/web/src/router.tsx
+++ bps-backend/apps/web/src/router.tsx
@@ -1,9 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
 
-export const router = createBrowserRouter([
-    {
-        path: "/signup",
+export const router = createBrowserRouter([{
+        path: "/auth/signup",
         element: <Signup />,
-    },
+    },{
+        path: "/auth/signin",
+        element: <Signin />,
+    }
 ]);
